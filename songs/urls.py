90e284fb0fa9efcae2bde5ea16495598ee00e98b
arshbhamra <arshbhambra88@gmail.com>
2026-09-08 11:38:33 +0530

    added Play song

--- songs/urls.py
+++ songs/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.song_list, name="song_list"),
     path("add/", views.add_song, name="add_song"),
     path("edit/<int:song_id>/", views.edit_song, name="edit_song"),
+        path('play/', views.play_song, name='play_song'),  # <-- ADD THIS
+
 ]
