c98253192215b93794a53697119f5b39c870b08a
noname <147178562+arshbhamra@users.noreply.github.com>
2026-09-12 10:36:02 +0530

    admin added in urls
    Added a URL pattern for playing songs.

--- songs/urls.py
+++ songs/urls.py
@@ -1,10 +1,12 @@
 # songs/urls.py
 
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.song_list, name="song_list"),
+    path("admin/", admin.site.urls),
     path("add/", views.add_song, name="add_song"),
     path("edit/<int:song_id>/", views.edit_song, name="edit_song"),
     path('play/', views.play_song, name='play_song'),  # <-- ADD THIS
